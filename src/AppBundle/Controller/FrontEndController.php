60a87e1e691d12ad0d2d3aa7f28b35a3f53343bc
ihorkruchynenko <ihorkruchynenko@10.118.109.65>
2016-06-29 21:30:32 +0200

    Great commit

---
+++ src/AppBundle/Controller/FrontEndController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class FrontEndController extends Controller
+{
+    /**
+     * @Route("/", name="index")
+     */
+    public function indexAction(Request $request)
+    {
+        return $this->render('FrontEnd/index.html.twig');
+    }
+    
+    /**
+     * @Route("terms", name="terms")
+     */
+    public function termsAction(Request $request)
+    {
+        return $this->render('FrontEnd/terms.html.twig');
+    }
+    
+    /**
+     * @Route("privacy", name="privacy")
+     */
+    public function privacyAction(Request $request)
+    {
+        return $this->render('FrontEnd/privacy.html.twig');
+    }
+  
+}
