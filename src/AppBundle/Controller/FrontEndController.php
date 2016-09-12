aba9546eb031f195f679d7cd665d4b34c72b90a5
ihorkruchynenko <ihorkruchynenko@10.118.109.65>
2016-09-12 11:49:33 +0600

    Great commit

--- src/AppBundle/Controller/FrontEndController.php
+++ src/AppBundle/Controller/FrontEndController.php
@@ -164,7 +164,7 @@ class FrontEndController extends Controller
             $newContact ->setMessage($message);
             $em->persist($newContact);
             $em->flush();
-                    
+            
              //create email
 
              $message = Swift_Message::newInstance()
